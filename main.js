b2fe8991fa76fd56ce39e789d074e852b8787e06
webheadd <rodolfo_stamaria_2008@yahoo.com>
2019-06-09 20:00:36 +0800

    Portfolio v1.3

---
+++ main.js
@@ -0,0 +1,107 @@
+var menuIcon = document.querySelector("#burger");
+var navMenu = document.querySelector("#nav");
+var closeBtn = document.querySelector("#closeBtn");
+
+var navLinks = document.querySelectorAll(".nav__link");
+
+var sec_head = document.querySelectorAll(".section__header");
+var folio_li = document.querySelectorAll(".folio__li");
+var about_desc = document.querySelectorAll(".about__desc");
+var about_img = document.querySelectorAll(".about__img");
+var contact_form = document.querySelectorAll(".contact__animate");
+var arr = Array.from(navLinks);
+
+
+//animate when scrolled
+window.addEventListener('scroll', animate);
+var currentPosition = window.innerHeight / 1.2;
+    console.log(currentPosition);
+function animate(){
+    header();
+    folio();
+    aboutDesc();
+    aboutImg();
+    contact();
+}
+//animate section headers
+function header() {
+    for(var i = 0; i < sec_head.length; i++){
+        var targetPosition = [];
+        targetPosition[i] = sec_head[i].getBoundingClientRect().top;
+        //console.log(targetPosition[0]);
+        if(targetPosition[i] < currentPosition){
+            sec_head[i].classList.add("scroll");  
+        }
+    }
+}
+//portfolio
+function folio() {
+    for(var i = 0; i < folio_li.length; i++){
+        var targetPosition = [];
+        targetPosition[i] = folio_li[i].getBoundingClientRect().top;
+        //console.log(targetPosition[0]);
+        if(targetPosition[i] < currentPosition){
+            folio_li[i].classList.add("scroll");  
+        }
+    }
+}
+//about paragraphs
+function aboutDesc() {
+    for(var i = 0; i < about_desc.length; i++){
+        var targetPosition = [];
+        targetPosition[i] = about_desc[i].getBoundingClientRect().top;
+        //console.log(targetPosition[0]);
+        if(targetPosition[i] < currentPosition){
+            about_desc[i].classList.add("left");  
+        }
+    }
+}
+//about img's
+function aboutImg() {
+    for(var i = 0; i < about_img.length; i++){
+        var targetPosition = [];
+        targetPosition[i] = about_img[i].getBoundingClientRect().top;
+        //console.log(targetPosition[0]);
+        if(targetPosition[i] < currentPosition){
+            about_img[i].classList.add("left");  
+        }
+    }
+}
+//contact form
+function contact() {
+    for(var i = 0; i < contact_form.length; i++){
+        var targetPosition = [];
+        targetPosition[i] = contact_form[i].getBoundingClientRect().top;
+        //console.log(targetPosition[0]);
+        if(targetPosition[i] < currentPosition){
+            contact_form[i].classList.add("left");  
+        }
+    }
+}
+arr.forEach(element => {
+    element.addEventListener('click', (e) => {
+        e.preventDefault();
+        //get href value
+        let linkValue = e.target.getAttribute("href");
+        
+        //grab href values to the dom as ids then scroll to it
+        var link_href = document.querySelector(linkValue).scrollIntoView({
+            behavior: 'smooth'
+        });
+        //close navMenu when anchor link is clicked
+        if(navMenu.classList.contains("slide")) navMenu.classList.remove("slide");
+    });
+});
+
+
+document.addEventListener('click', slideIn);
+
+
+//navigation slide in
+function slideIn(e) {
+    if(e.target.id == "burger" || e.target.closest("#burger > span")){
+        navMenu.classList.add("slide");
+    } else if(e.target.id == "closeBtn" || e.target.closest("#closeBtn > span")){
+        navMenu.classList.remove("slide");
+    }
+}
